b3ceea1a6ebe9416fa8257ecaf9223e3236e0368
shobhitrawat <shobhitrawat@gmail.com>
2022-03-03 16:18:35 +0530

    keepalived codes

--- roles/configuration/templates/scripts/redis-7501/redis_master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REDISCLI="/usr/local/bin/redis-cli"
-LOGFILE="/var/log/keepalived/keepalived-redis-state.log"
-echo "[master]" >> $LOGFILE
-date >> $LOGFILE
-echo "Being master...." >> $LOGFILE 2>&1
-echo "Run SLAVEOF NO ONE cmd ..." >> $LOGFILE
-$REDISCLI SLAVEOF NO ONE >> $LOGFILE 2>&1
